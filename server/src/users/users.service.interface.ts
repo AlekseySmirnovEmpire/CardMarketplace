5a9ce984b29e1ce831e36081bd238d95d92fc63e
Aleksey Smirnov <i@smirnov-alexsey.ru>
2023-02-10 09:16:13 +0300

    Upgrade

--- server/src/users/users.service.interface.ts
+++
@@ -1,9 +0,0 @@
-import { UserRegisterDto } from './dto/user-register.dto';
-import { UserLoginDto } from './dto/user-login.dto';
-import { UserModel } from '@prisma/client';
-import { User } from './user.entity';
-
-export interface IUserService {
-  createUser: (dto: UserRegisterDto) => Promise<UserModel | null>;
-  validateUser: (dto: UserLoginDto) => Promise<User | null>;
-}
